79511e3de6c95db32bcdff37da94c529e21ec933
DerekSlagle <58993653+DerekSlagle@users.noreply.github.com>
2025-07-02 20:40:05 -0500

    Add files via upload

--- Light_types/DataPlayer.cpp
+++ Light_types/DataPlayer.cpp
@@ -1,33 +1,34 @@
 #include "DataPlayer.h"
 
-void DataPlayer::init( Light& r_Lt0, int Rows, int Cols, uint8_t& r_StateData, unsigned int DataSz, uint8_t NumColors )
+void DataPlayer::init( Light& r_Lt0, int GridRows, int GridCols, uint8_t& r_StateData, unsigned int DataSz, uint8_t NumColors )
 {
     pLt0 = &r_Lt0;
-    rows = Rows;
-    cols = Cols;
-    numLts = rows*cols;
+    rows = GridRows;
+    cols = GridCols;
 
     stepTimer = 0;
     stepIter = 0;
 
     // default is entire grid
-    gridRows = rows;
-    gridCols = cols;
+    gridRows = GridRows;
+    gridCols = GridCols;
     row0 = col0 = 0;
-    drawMode = 1;// is grid
 
     pStateData = &r_StateData;
     stateDataSz = DataSz;
     numColors = NumColors;
     BA.init( r_StateData, DataSz );
 
+    const int NumLts = rows*cols;
 
-    if( numColors == 2 )
-        numSteps = (8*DataSz)/numLts;
-    else if( numColors == 4 )
-        numSteps = (4*DataSz)/numLts;
-    else if( numColors == 16 )
-        numSteps = (2*DataSz)/numLts;
+    if( numColors > 8 && numColors <= 16 )
+        numSteps = (2*DataSz)/NumLts;
+    else if( numColors > 4 && numColors <= 8 )// new
+        numSteps = (8*DataSz)/(3*NumLts);
+    else if( numColors > 2 )// 3 or 4
+        numSteps = (4*DataSz)/NumLts;
+    else if( numColors == 2 )
+        numSteps = (8*DataSz)/NumLts;
 }
 
 void DataPlayer::setGridBounds( int Row0, int Col0, int GridRows, int GridCols )
@@ -36,8 +37,6 @@ void DataPlayer::setGridBounds( int Row0, int Col0, int GridRows, int GridCols )
     col0 = Col0;
     gridRows = GridRows;
     gridCols = GridCols;
-
-    setDrawMode();
 }
 
 void DataPlayer::bindToGrid( Light& r_Lt0, int GridRows, int GridCols )
@@ -52,17 +51,16 @@ void DataPlayer::setTargetRect( int Rows, int Cols, int Row0, int Col0 )
     cols = Cols;
     row0 = Row0;
     col0 = Col0;
-    setDrawMode();
 }
 
-void DataPlayer::setDrawMode()
+int DataPlayer::getDrawMode()const
 {
     if( rows == gridRows && cols == gridCols && row0 == 0 && col0 == 0 )
-        drawMode = 1;// is grid
-    else if( ( row0 >= 0 && row0 + rows <= gridRows ) && ( col0 >= 0 && col0 + cols <= gridCols ) )
-        drawMode = 2;// is all in grid
-    else
-        drawMode = 3;// is partly in grid
+        return 1;// is grid
+    if( ( row0 >= 0 && row0 + rows <= gridRows ) && ( col0 >= 0 && col0 + cols <= gridCols ) )
+        return 2;// is all in grid
+
+    return 3;// is partly in grid
 }
 
 void DataPlayer::takeStep()
@@ -77,9 +75,26 @@ void DataPlayer::takeStep()
     }
 }
 
+void DataPlayer::Clear( Light clearLt )const
+{
+    Light* pBase = pLt0 + gridCols*row0 + col0;
+    for( int r = 0; r < rows; ++r )
+    {
+        if( r + row0 < 0 ) continue;
+        if( r + row0 >= gridRows ) break;
+        Light* pRow = pBase + r*gridCols;
+        for( int c = 0; c < cols; ++c )
+        {
+            if( c + col0 < 0 ) continue;
+            if( c + col0 >= gridCols ) break;
+            *( pRow + c ) = clearLt;
+        }
+    }
+}
+
 void DataPlayer::updateIsGrid()// 1
 {
-    for( unsigned int n = 0; n < numLts; ++n )
+    for( int n = 0; n < rows*cols; ++n )
     {
         if( fadeAlong )
             *( pLt0 + n ) = updateFade(n);
@@ -87,7 +102,7 @@ void DataPlayer::updateIsGrid()// 1
         {
             Light LtNow = getState(n);
             if( drawOff ) *( pLt0 + n ) = LtNow;// draw all colors
-            else if( LtNow != Lt[0] ) *( pLt0 + n ) = LtNow;// draw all but Lt[0]
+            else if( LtNow != pLight[0] ) *( pLt0 + n ) = LtNow;// draw all but Lt[0]
         }
     }
 
@@ -108,7 +123,7 @@ void DataPlayer::updateAllIn()// 2
             {
                 Light LtNow = getState( r*cols + c );
                 if( drawOff ) *( pRow + c ) = LtNow;// draw all colors
-                else if( LtNow != Lt[0] ) *( pRow + c ) = LtNow;// draw all but Lt[0]
+                else if( LtNow != pLight[0] ) *( pRow + c ) = LtNow;// draw all but Lt[0]
             }
         }
     }
@@ -129,7 +144,7 @@ void DataPlayer::flipX_AllIn()
             {
                 Light LtNow = getState( r*cols + cols - 1 - c );
                 if( drawOff ) *( pRow + c ) = LtNow;// draw all colors
-                else if( LtNow != Lt[0] ) *( pRow + c ) = LtNow;// draw all but Lt[0]
+                else if( LtNow != pLight[0] ) *( pRow + c ) = LtNow;// draw all but Lt[0]
             }
         }
     }
@@ -150,7 +165,7 @@ void DataPlayer::flipY_AllIn()
             {
                 Light LtNow = getState( ( rows - 1 - r )*cols + c );
                 if( drawOff ) *( pRow + c ) = LtNow;// draw all colors
-                else if( LtNow != Lt[0] ) *( pRow + c ) = LtNow;// draw all but Lt[0]
+                else if( LtNow != pLight[0] ) *( pRow + c ) = LtNow;// draw all but Lt[0]
             }
         }
     }
@@ -178,7 +193,7 @@ void DataPlayer::updatePartlyIn()// 3
             {
                 Light LtNow = getState( r*cols + c );
                 if( drawOff ) *( pRow + c ) = LtNow;// draw all colors
-                else if( LtNow != Lt[0] ) *( pRow + c ) = LtNow;// draw all but Lt[0]
+                else if( LtNow != pLight[0] ) *( pRow + c ) = LtNow;// draw all but Lt[0]
             }
         }
     }
@@ -188,44 +203,51 @@ void DataPlayer::updatePartlyIn()// 3
 
 void DataPlayer::update()
 {
+    int DrawMode = getDrawMode();
     if( flipX )
     {
-        if( drawMode == 1 || drawMode == 2 ) flipX_AllIn();
+        if( DrawMode == 1 || DrawMode == 2 ) flipX_AllIn();
         else updatePartlyIn();// no flip is done here
     }
     else if( flipY )
     {
-        if( drawMode == 1 || drawMode == 2 ) flipY_AllIn();
+        if( DrawMode == 1 || DrawMode == 2 ) flipY_AllIn();
         else updatePartlyIn();// no flip is done here
     }
     else
     {
-        if( drawMode == 1 ) updateIsGrid();
-        else if( drawMode == 2 ) updateAllIn();
+        if( DrawMode == 1 ) updateIsGrid();
+        else if( DrawMode == 2 ) updateAllIn();
         else updatePartlyIn();
     }
+
+    // after process. Adjust brightness
+    if( pBrtFactor )
+        updateBrigtness( *pBrtFactor );
 }
 
 Light DataPlayer::updateFade( unsigned int n )const
 {
-    Light LtNow = Lt[0], LtNext = Lt[0];
+    if( !pLight ) return Light(0,200,0);
+
+    Light LtNow = pLight[0], LtNext = pLight[0];
     unsigned int iterNext = ( stepIter + 1 )%numSteps;
+    const int NumLts = rows*cols;
 
-
-    if( numColors == 2 )// 2 color
+    if( numColors > 4 && numColors <= 16 )
     {
-        LtNow = BA.getBit( stepIter*numLts + n ) ? Lt[1] : Lt[0];
-        LtNext = BA.getBit( iterNext*numLts + n ) ? Lt[1] : Lt[0];
+        LtNow = pLight[ BA.getQuadBit( stepIter*NumLts + n ) ];
+        LtNext = pLight[ BA.getQuadBit( iterNext*NumLts + n ) ];
     }
-    else if( numColors == 4 )
+    else if( numColors > 2 )// 3 or 4
     {
-        LtNow = Lt[ BA.getDblBit( stepIter*numLts + n ) ];
-        LtNext = Lt[ BA.getDblBit( iterNext*numLts + n ) ];
+        LtNow = pLight[ BA.getDblBit( stepIter*NumLts + n ) ];
+        LtNext = pLight[ BA.getDblBit( iterNext*NumLts + n ) ];
     }
-    else if( numColors == 16 )
+    else if( numColors == 2 )// 2 color
     {
-        LtNow = Lt[ BA.getQuadBit( stepIter*numLts + n ) ];
-        LtNext = Lt[ BA.getQuadBit( iterNext*numLts + n ) ];
+        LtNow = BA.getBit( stepIter*NumLts + n ) ? pLight[1] : pLight[0];
+        LtNext = BA.getBit( iterNext*NumLts + n ) ? pLight[1] : pLight[0];
     }
 
     if( LtNow == LtNext ) return LtNow;
@@ -272,28 +294,70 @@ void DataPlayer::updateZoomOut()// 3
 
             Light LtNow( rd/4.0f, gn/4.0f, bu/4.0f );
             if( drawOff ) *( pRow + c/2 ) = LtNow;// draw all colors
-            else if( LtNow != Lt[0] ) *( pRow + c/2 ) = LtNow;// draw all but Lt[0]
+            else if( LtNow != pLight[0] ) *( pRow + c/2 ) = LtNow;// draw all but Lt[0]
         }
     }
 
     takeStep();
 }
 
+// safe. Good for partly in
+void DataPlayer::updateBrigtness( float brtFactor )
+{
+    if( brtFactor <= 0.0f ) return;// nonsense value
+
+    Light* pBase = pLt0 + gridCols*row0 + col0;
+
+    for( int r = 0; r < rows; ++r )
+    {
+        if( r + row0 < 0 ) continue;
+        if( r + row0 >= gridRows ) break;
+
+        Light* pRow = pBase + r*gridCols;
+        for( int c = 0; c < cols; ++c )
+        {
+            if( c + col0 < 0 ) continue;
+            if( c + col0 >= gridCols ) break;
+
+            Light LtNow = getState( r*cols + c );
+            if( LtNow != pLight[0] )// fade only non background colors
+            {
+                float fCol = LtNow.r*brtFactor;
+                if( fCol > 255.0f ) fCol = 255.0f;// clamp value
+                LtNow.r = fCol;
+                fCol = LtNow.g*brtFactor;
+                if( fCol > 255.0f ) fCol = 255.0f;
+                LtNow.g = fCol;
+                fCol = LtNow.b*brtFactor;
+                if( fCol > 255.0f ) fCol = 255.0f;
+                LtNow.b = fCol;
+                *( pRow + c ) = LtNow;// draw all but Lt[0]
+            }
+        }
+    }
+}
+
 Light DataPlayer::getState( unsigned int n )const
 {
+    if( !pLight ) return Light(200,0,0);
+    const int NumLts = rows*cols;
+
+    if( numColors > 8 && numColors <= 16 )
+        return pLight[ BA.getQuadBit( stepIter*NumLts + n ) ];
+
+    if( numColors > 4 && numColors <= 8 )// new case
+        return pLight[ BA.getTriBit( stepIter*NumLts + n ) ];
+
+    if( numColors > 2 )// 3 or 4
+        return pLight[ BA.getDblBit( stepIter*NumLts + n ) ];
+
     if( numColors == 2 )
     {
-        if( BA.getBit( stepIter*numLts + n ) ) return Lt[1];
-        return Lt[0];
+        if( BA.getBit( stepIter*NumLts + n ) ) return pLight[1];
+        return pLight[0];
     }
 
-    if( numColors == 4 )
-        return Lt[ BA.getDblBit( stepIter*numLts + n ) ];
-
-    if( numColors == 16 )
-        return Lt[ BA.getQuadBit( stepIter*numLts + n ) ];
-
-    return Lt[0];
+    return pLight[0];
 }
 
 void DataPlayer::prevImage()
@@ -316,12 +380,122 @@ void DataPlayer::showImage( unsigned int n )// 0 to numSteps - 1
     stepIter = n%numSteps;// keep in range
 }
 
-// on grid only
+// For testing. on grid only
 void DataPlayer::showColors()const
 {
-    for( unsigned int n = 0; n < numLts; ++n )
+    for( int n = 0; n < rows*cols; ++n )
     {
-        if( n < numColors ) *( pLt0 + n ) = Lt[n];
-        else *( pLt0 + n ) = Lt[0];
+        if( n < (int)numColors ) *( pLt0 + n ) = pLight[n];
+        else *( pLt0 + n ) = pLight[0];
+    }
+}
+
+void DataPlayer::updateRainfall( int (*pGetCol)(void) )
+{
+    if( stepTimer == 0 )// shift states down 1 row
+    {
+        for( int c = 0; c < cols; ++c )
+        {
+            for( int r = rows - 1; r > 0; --r )// up from bottom
+            {
+                BA.setBit( r*cols + c, BA.getBit( (r-1)*cols + c ) );// from row above to row below
+            }
+            // write all 0's to row 0
+            BA.setBit( c, false );
+        }
+        // write new raindrops to row 0
+        if( pGetCol() )
+        {
+            int numDrops = pGetCol()%3;
+            for( int d = 0; d < numDrops; ++d )
+            {
+                int C = pGetCol()%cols;
+                if( !BA.getBit( cols + C ) )// is 0 one row down
+                    BA.setBit( C, true );
+            }
+        }
+    }
+
+    update();// normally
+}
+
+void DataPlayer::updateRainfall_fade( int (*pGetCol)(void), bool doEase )
+{
+    takeStep();
+
+    if( stepTimer == 0 )// shift states down 1 row
+    {
+        for( int c = 0; c < cols; ++c )
+        {
+            for( int r = rows - 1; r > 0; --r )// up from bottom
+                BA.setBit( r*cols + c, BA.getBit( (r-1)*cols + c ) );// from row above to row below
+
+            // write all 0's to row 0
+            BA.setBit( c, false );
+        }
+        // write new raindrops to row 0
+        if( pGetCol() )
+        {
+            int numDrops = pGetCol()%3;
+            for( int d = 0; d < numDrops; ++d )
+            {
+                int C = pGetCol()%cols;
+                if( !BA.getBit( cols + C ) )// is 0 one row down
+                    BA.setBit( C, true );
+            }
+        }
+    }
+
+    // fade each row into the next row
+    float U = (float)stepTimer/(float)stepPause;// interpolation factor
+    if( doEase ) U = U*U*( 3.0f - 2.0f*U );// easing
+    Light* pBase = pLt0 + row0*gridCols + col0;
+
+    for( int c = 0; c < cols; ++c )
+    {
+        if( drawOff )
+        {
+            for( int r = 0; r < rows; ++r )
+                *( pBase + r*gridCols + c ) = pLight[0];
+        }
+
+        for( int r = 0; r + 1 < rows; ++r )// down from top
+        {
+            int ofst_0 = (r+row0)*gridCols + col0 + c;// row above
+
+            if( !BA.getBit( r*cols + c ) )// draw pLight[0]
+            {
+           //     if( drawOff ) pLt0[ ofst_0 ] = pLight[0];
+                continue;
+            }
+
+            // a raindrop is at r,c
+            Light Lt_0 = drawOff ? pLight[0] : pLt0[ ofst_0 ];// bkgd or existing Light
+            // write to Light in each row
+            pLt0[ ofst_0 ].r = U*Lt_0.r + ( 1.0f - U )*pLight[1].r;
+            pLt0[ ofst_0 ].g = U*Lt_0.g + ( 1.0f - U )*pLight[1].g;
+            pLt0[ ofst_0 ].b = U*Lt_0.b + ( 1.0f - U )*pLight[1].b;
+
+            int ofst_1 = (r+1+row0)*gridCols + col0 + c;// row below
+       //     Light Lt_1 = BA.getBit( (r+1)*cols + c ) ? pLight[1] : pLight[0];// current assigned state
+            Light Lt_1 =  drawOff ? pLight[0] : pLt0[ ofst_1 ];// current assigned state
+            pLt0[ ofst_1 ].r = ( 1.0f - U )*Lt_1.r + U*pLight[1].r;
+            pLt0[ ofst_1 ].g = ( 1.0f - U )*Lt_1.g + U*pLight[1].g;
+            pLt0[ ofst_1 ].b = ( 1.0f - U )*Lt_1.b + U*pLight[1].b;
+        }
+
+
+        // fade the last row drops ?
+        int ofst = (rows-1+row0)*gridCols + col0 + c;
+     //   if( drawOff ) pLt0[ ofst ] = pLight[0];
+        Light Lt_2 =  drawOff ? pLight[0] : pLt0[ ofst ];
+        if( BA.getBit( (rows - 1)*cols + c ) )
+        {
+            pLt0[ ofst ].r = U*Lt_2.r + ( 1.0f - U )*pLight[1].r;
+            pLt0[ ofst ].g = U*Lt_2.g + ( 1.0f - U )*pLight[1].g;
+            pLt0[ ofst ].b = U*Lt_2.b + ( 1.0f - U )*pLight[1].b;
+        }
+    //    else if( drawOff )
+    //        pLt0[ ofst ] = pLight[0];
     }
 }
--- Light_types/DataPlayer.h
+++ Light_types/DataPlayer.h
@@ -2,43 +2,32 @@
 #define DATAPLAYER_H
 
 #include "Light.h"
-#include "../utility/bitArray.h"
 
-// full data set for changing which image is drawn
-struct DPdata
-{
-    uint8_t* pStateData = nullptr;
-    unsigned int stateDataSz = 0;
-    Light Lt[16];
-    uint8_t numColors = 2;// 2, 4 or 16
-    bool drawOff = true;// draw both if true or draw only onLt if false
-    bool fadeAlong = false;// fade each frame into the next
-    int rows, cols, row0, col0;
-};
+//#include "C_RGB.h"
+#include "../utility/bitArray.h"
+// new
+//#include "../Light_types/LightPalette.h"
 
 class DataPlayer
 {
-    protected:
-    Light* pLt0 = nullptr;
+    public:
+    Light* pLt0 = nullptr;// the target Light grid
     int rows = 1, cols = 1;
     int row0 = 0, col0 = 0;// origin in grid
     int gridCols = 1, gridRows = 1;// bounding grid
-    // dependent. For convenience in functions
-    unsigned int numLts = 1;// numLts = rows*cols
-    int drawMode = 3;// 1: is grid, 2: is all in grid, 3: is partly in grid
-    void setDrawMode();
+ //   int drawMode = 3;// 1: is grid, 2: is all in grid, 3: is partly in grid
+    int getDrawMode()const;// to replace drawMode member
     void updateIsGrid();// 1
     void updateAllIn();// 2
     void updatePartlyIn();// 3
 
-    public:
-    Light Lt[16];// use 0 and 1 for 2 colors
-    bool drawOff = true;// draw both if true or draw only onLt if false
+ //   public:
+    bool drawOff = true;// draw pLight[0] if true. Background color
     bool fadeAlong = false;// fade each frame into the next
-    Light updateFade( unsigned int n )const;
+    Light updateFade( unsigned int n )const;// called by update() when above is true
 
     // play pause seek functions
-    bool isPlaying = true;
+    bool isPlaying = true;// stops animation. Drawing continues
     void reStart(){ stepTimer = stepIter = 0; }
     void prevImage();
     void nextImage();
@@ -47,41 +36,38 @@ class DataPlayer
     // storage for bitArray
     uint8_t* pStateData = nullptr;
     unsigned int stateDataSz = 0;
-    // newest
-    bitArray BA;// for bitwise storage of above stateData
-    uint8_t numColors = 2;// 2, 4 or 16
+
+    // array storage
+    Light* pLight = nullptr;// to array of Lights defining the colors
+    bitArray BA;// for bitwise storage of index to Light color for each state assignment
+    unsigned int numColors = 2;// 2 to 16
+    Light getState( unsigned int n )const;
 
     // transform image
-    bool flipX = false, flipY = false;
+    bool flipX = false, flipY = false;//
     void flipX_AllIn();// for drawMode = 1 or 2
     void flipY_AllIn();// for drawMode = 1 or 2
-
 
     // state management
     unsigned int stepTimer = 0, stepPause = 1;// timer for stepIter incrementation
     unsigned int stepIter = 0, numSteps = 1;
 
-    void update();
+    // user to call update()
+    void update();// write Light states to area in grid and takeStep()
     void updateZoomOut();
-    void takeStep();
-
-    Light getState( unsigned int n )const;
+    void takeStep();// called by update()
+    void updateBrigtness( float brtFactor );// brtFactor = 1.0f is normal brightness. 1.5f = 50% brighter, 0.5f = half bright
+    const float* pBrtFactor = nullptr;// assign to trigger calls to above from update()
+    // special usage
+    void updateRainfall( int (*pGetCol)(void) );
+    void updateRainfall_fade( int (*pGetCol)(void), bool doEase = false );
 
     // test or utility
     void showColors()const;// display in order 0 to numLts - 1
+    void Clear( Light clearLt )const;
+    const Light* get_pLt0()const { return pLt0; }
 
-    int getRows()const{ return rows; }
-    int getCols()const{ return cols; }
-    int getRow0()const{ return row0; }
-    int getCol0()const{ return col0; }
-    unsigned int getNumLts()const{ return numLts; }
-    // setters
-    void setRows( int Rows ){ rows = Rows; setDrawMode(); }
-    void setCols( int Cols ){ cols = Cols; setDrawMode(); }
-    void setRow0( int Row0 ){ row0 = Row0; setDrawMode(); }
-    void setCol0( int Col0 ){ col0 = Col0; setDrawMode(); }
-
-    void init( Light& r_Lt0, int Rows, int Cols, uint8_t& r_StateData, unsigned int DataSz, uint8_t NumColors );
+    void init( Light& r_Lt0, int GridRows, int GridCols, uint8_t& r_StateData, unsigned int DataSz, uint8_t NumColors );
     void bindToGrid(  Light& r_Lt0, int GridRows, int GridCols );
     void setGridBounds( int Row0, int Col0, int GridRows, int GridCols );
     void setTargetRect( int Rows, int Cols, int Row0, int Col0 );
