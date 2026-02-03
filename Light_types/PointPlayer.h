db180ba5ea70d48e81c8acf6ca0ddc0e7bd870b0
DerekSlagle <58993653+DerekSlagle@users.noreply.github.com>
2026-02-02 21:19:45 -0800

    Add files via upload

--- Light_types/PointPlayer.h
+++ Light_types/PointPlayer.h
@@ -18,27 +18,39 @@ class PointPlayer
     bool isPlaying = false;
 
     // zoomie traits
-    vec2f pos0, pos, vel;
+    vec2f pos0, pos;// vel;
     float speed = 10.0f;// leds per second
     Light ptColor;
  //   float tPeriod = 3.0f, tFade = 1.0f, tElap = 0.0f;
     // a path to visit on grid only
     uint8_t* pathX = nullptr, *pathY = nullptr;// parallel arrays
-    uint8_t numPoints = 0, currPoint = 0, nextPoint = 0;
+    uint8_t numPoints = 0, currPoint = 0;// nextPoint = 0;
     float currLength = 0.0f;// distance to next point
     float fadeLength = 8.0f;// leaves a trail
-    vec2f uCurr, uPrev;// assign on switch over
+    vec2f uCurr;// uPrev;// assign on switch over
+    // Grow from a point and shrink to a point
+    int growState = 0;// 1: growing, 0: Normal (in use), -1: shrinking
+    float growLength = 0.0f;// for both growing and shrinking
 
     void assignPath( const char* fileName );
     void setup( uint8_t& pathX0, uint8_t& pathY0, uint8_t NumPoints, float speed, Light PtColor );
     void Start();
+    void Stop();
 
-    bool update( float dt );// true if animating
- //   void update_tElapOnly( float dt );
+    bool update( float dt );// true if animating 
     void draw()const;// update() = time update
+    // try Bresenham's algorithm
+    void draw2()const;// for all segments
+    // per segment
+    void drawLine( int8_t x0, int8_t y0, int8_t x1, int8_t y1, Light color )const;
+    // helper to rasterize
+    
+    void draw3()const;
 
     PointPlayer(){}
     ~PointPlayer(){}
 };
+
+vec2f get_dIter( vec2f line );// step along line
 
 #endif // POINTPLAYER_H
